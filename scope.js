0bd54cf3f0c88ad46d22cd8855471a7cbea11250
fahim-ahmed-7861 <fahimahmedshojib109@gmail.com>
2020-03-13 23:57:29 +0600

    done

---
+++ scope.js
@@ -0,0 +1,30 @@
+let res;
+
+function sum(a,b)
+{
+
+    res=a+b;
+
+   
+
+}
+
+ sum(10,22);
+
+ var ans=res;
+
+ {
+     {
+         {
+             var a=10;
+         }
+
+         console.log(a);
+     }
+
+     console.log(a);
+ }
+
+ 
+
+ console.log(ans);
