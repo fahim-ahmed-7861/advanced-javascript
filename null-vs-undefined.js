3f9cef6fe0959472721d1dc4fcd21ccbf5980f85
fahim-ahmed-7861 <fahimahmedshojib109@gmail.com>
2020-03-13 20:04:11 +0600

    null vs undefined for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+console.log(pakhi);
+
+function print(a,b)
+{
+    console.log(a,b); 
+}
+
+print(10);
+
+
+const a={name:"Fahim Ahmed",age:23};
+
+console.log(a.mobile_no);
+
+let fun=undefined;
+
+console.log(fun);
+
